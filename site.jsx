d346144777e90b4391415c5d4b6f5f883db6322b
arsalanahmadloo <arsalanahmadloo13890331@gmail.com>
2025-05-26 14:23:14 +0330

    Delete site.jsx

--- site.jsx
+++
@@ -1,7 +0,0 @@
-export default function SiteSection() {
-  return (
-    <div className="min-h-screen bg-gradient-to-r from-green-400 to-blue-600 flex items-center justify-center text-white text-3xl font-bold">
-      خوش آمدید به بخش سایت!
-    </div>
-  );
-}
